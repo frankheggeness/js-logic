a5c96b9d8a2288846da6c60ccee341f01927c1a7
unknown <fr.heggeness@gmail.com>
2019-02-11 14:09:10 -1000

    q16

--- exercises.js
+++ exercises.js
@@ -367,7 +367,9 @@ for (var i = 0; i<players.length; i++){
  * Create a for loop that will iterate and console.log each item in the array below:
 */
   var myFavFoods = ["lemon bar", "carrot cake", "nachos", "bacon cheeseburger", "ramen", "sweet potato fries", "chimichanga"];
-
+for (var i = 0; i<myFavFoods.length; i++){
+  console.log(myFavFoods[i])
+}
 
 /*
  * #17
